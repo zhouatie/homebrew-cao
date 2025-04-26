ee61033381615c54782a79cd5c81c2a6e11bdfff
zhouatie <zhouatie@gmail.com>
2025-04-26 20:01:46 +0800

    移除对 python@3.10 的强制依赖，改为使用通用 python 依赖

--- cao.rb
+++ cao.rb
@@ -7,7 +7,7 @@ class Cao < Formula
   sha256 "e21c7e8969cf06e7a3b6a2a6edbb85ef9e19a85321841cd120e0208e640d3ed9"
   license "MIT"
 
-  depends_on "python@3.10"
+  depends_on "python"
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
